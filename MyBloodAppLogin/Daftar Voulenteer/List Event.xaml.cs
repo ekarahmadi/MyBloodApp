b7a97901ad50aa4adf6ed67106bb5bf31d92fd5b
BrianTirafiA <114814776+BrianTirafiA@users.noreply.github.com>
2023-12-06 23:02:09 +0700

    add list event

---
+++ MyBloodAppLogin/Daftar Voulenteer/List Event.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyBloodAppLogin.Daftar_Voulenteer
+{
+    /// <summary>
+    /// Interaction logic for List_Event.xaml
+    /// </summary>
+    public partial class List_Event : Page
+    {
+        public List_Event()
+        {
+            InitializeComponent();
+        }
+    }
+}
