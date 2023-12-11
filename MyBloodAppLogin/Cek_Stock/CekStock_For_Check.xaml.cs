1dea1ba6e5687a7aee39f8674891263b66128096
Septian Eka Rahmadi <septianekarahmadi@mail.ugm.ac.id>
2023-12-12 02:11:58 +0700

    Add CekStock Folder

---
+++ MyBloodAppLogin/Cek_Stock/CekStock_For_Check.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyBloodAppLogin.Cek_Stock
+{
+    /// <summary>
+    /// Interaction logic for CekStock_For_Check.xaml
+    /// </summary>
+    public partial class CekStock_For_Check : Page
+    {
+        public CekStock_For_Check()
+        {
+            InitializeComponent();
+        }
+    }
+}
